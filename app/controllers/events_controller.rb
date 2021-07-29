0c1172bffb6ecbe4e9d1a4065371cb561089e1a9
gnpashi <guynatan@gmail.com>
2021-07-30 01:51:28 +0300

    destroy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,7 +25,7 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to events_url, notice: "Event was successfully created." }
+        format.html { redirect_to root_path, notice: "Event was successfully created." }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to events_url, notice: "Event was successfully updated." }
+        format.html { redirect_to root_path, notice: "Event was successfully updated." }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,7 +51,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.html { redirect_to root_path, notice: "Event was successfully destroyed." }
       format.json { head :no_content }
     end
   end
